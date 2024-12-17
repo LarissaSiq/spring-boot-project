dc92d147695e5b31974e1b29b92699f584e6a57b
Larissa Siqueira <larissa.silva@ab-inbev.com>
2024-12-17 11:09:21 -0300

    feat(): creating application with spring boot to find books

---
+++ src/main/java/com/livros/estudos/entity/Livro.java
@@ -0,0 +1,40 @@
+package com.livros.estudos.entity;
+
+import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "TB_LIVRO")
+public class Livro implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull
+  @Column(name = "titulo")
+  private String titulo;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+}
